66ef308ad1b739847a9b8241ce4289df85f732c9
José López <jslquintero@proton.me>
2024-04-19 20:16:47 -0500

    fixes

--- Code/dquestFrame.lua
+++ Code/dquestFrame.lua
@@ -262,3 +262,21 @@ QuestFrame:SetScript("OnShow",
         HandleAllPanels()
     end
 )
+
+
+local oldOnShow = QuestFrameGreetingPanel_OnShow;
+QuestFrameGreetingPanel_OnShow = function()
+    oldOnShow();
+
+    for i = 1, MAX_NUM_QUESTS do
+        local titleLine = getglobal("QuestTitleButton" .. i);
+        if (titleLine:IsVisible()) then
+            local bulletPointTexture = titleLine:GetRegions();
+            if (titleLine.isActive == 1) then
+                bulletPointTexture:SetTexture("Interface\\GossipFrame\\ActiveQuestIcon");
+            else
+                bulletPointTexture:SetTexture("Interface\\GossipFrame\\AvailableQuestIcon");
+            end
+        end
+    end
+end
